3ac1e9881e2b8b341256117d5fc91914af533a59
Dániel Zolnai <daniel.zolnai@cern.ch>
2016-09-14 12:18:39 +0200

    Final cleanup

--- app/src/main/java/net/tuxed/vpnconfigimporter/EduVPNApplication.java
+++ app/src/main/java/net/tuxed/vpnconfigimporter/EduVPNApplication.java
@@ -7,7 +7,8 @@ import de.blinkt.openvpn.core.PRNGFixes;
 import de.blinkt.openvpn.core.VpnStatus;
 
 /**
- * Created by Dani on 2016-09-14.
+ * Application object which keeps track of the app lifecycle.
+ * Created by Daniel Zolnai on 2016-09-14.
  */
 public class EduVPNApplication extends Application {
 
