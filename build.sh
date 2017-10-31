a1b82a231dbd0e33c4886a665f45fdecdeb9b57b
rogier <github@spoor.nu>
2017-10-31 13:36:21 +0100

    added install of android NDK and SDK
    Installation of required version of NDK/SDK in order to be able to create the app

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+# script created for CentOS 7
+# dependency: sudo
+# expected username: eduvpn
 
-# TODO: determine exact versions of the SDK/NDK required
     export ANDROID_HOME="/opt/android-sdk" 
     export PATH="$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:/opt/android-ndk:$PATH"
     export ANDROID_NDK=/opt/android-ndk
@@ -8,6 +10,26 @@
     #java home for openjdk
     export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")
 
+yes | sudo yum install java-1.8.0-openjdk-devel.x86_64 git
+#for Debian:
+#yes | sudo apt install openjdk-8-jdk git
+#Download Android SDK tools command-line
+mkdir ~/sdk-tools-linux-3859397; cd sdk-tools-linux-3859397
+wget https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip
+unzip sdk-tools-linux-3859397.zip
+sudo mv ~/sdk-tools-linux-3859397 /opt
+cd /opt; sudo ln -s sdk-tools-linux-3859397 android-sdk
+sudo chown -R root.eduvpn sdk-tools-linux-3859397
+sudo chmod g+w sdk-tools-linux-3859397/
+#all licenses accept
+yes | sudo /opt/android-sdk/tools/bin/sdkmanager --licenses
+#Install Android NDK
+cd ~;wget https://dl.google.com/android/repository/android-ndk-r15c-linux-x86_64.zip
+unzip android-ndk-r15c-linux-x86_64.zip
+sudo mv ./android-ndk-r15c /opt
+cd /opt
+sudo chown -R root.eduvpn android-ndk-r15c
+sudo ln -s android-ndk-r15c android-ndk
 
 # generate a keystore
 #keytool -genkey -v -keystore ~/my-release-key.jks
