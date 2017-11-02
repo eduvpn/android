db37227929a4ef66f76c1e9161abdc4ed0528e1c
François Kooman <fkooman@tuxed.net>
2017-11-02 10:11:01 +0100

    more cleanups

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 # script created for CentOS 7
 # dependency: sudo
 
+SDK_VERSION=3859397
+NDK_VERSION=r15c
+
 export ANDROID_HOME="/opt/android/sdk" 
 export PATH="$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:/opt/android/ndk:$PATH"
 export ANDROID_NDK=/opt/android/ndk
@@ -21,21 +24,19 @@ sudo chown "$(id -un).$(id -gn)" /opt/android
 # SDK
 mkdir -p /opt/android/sdk
 cd /opt/android/sdk || exit
-wget https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip
-unzip -q sdk-tools-linux-3859397.zip
-ln -s sdk-tools-linux-3859397 sdk
+wget https://dl.google.com/android/repository/sdk-tools-linux-${SDK_VERSION}.zip
+unzip -q sdk-tools-linux-${SDK_VERSION}.zip
+ln -s sdk-tools-linux-${SDK_VERSION} sdk
 
 yes | /opt/android/sdk/tools/bin/sdkmanager --licenses
 
 # NDK
 cd /opt/android || exit
-wget https://dl.google.com/android/repository/android-ndk-r15c-linux-x86_64.zip
-unzip -q android-ndk-r15c-linux-x86_64.zip
-ln -s android-ndk-r15c ndk
+wget https://dl.google.com/android/repository/android-ndk-${NDK_VERSION}-linux-x86_64.zip
+unzip -q android-ndk-${NDK_VERSION}-linux-x86_64.zip
+ln -s android-ndk-${NDK_VERSION} ndk
 
-# generate a keystore
-#keytool -genkey -v -keystore ~/my-release-key.jks
-
+# App Source Download
 rm -rf "${HOME}/eduvpn-app"
 cd "${HOME}" || exit
 git clone https://github.com/eduvpn/android.git eduvpn-app
@@ -43,7 +44,7 @@ git clone https://github.com/eduvpn/android.git eduvpn-app
 cd eduvpn-app || exit
 git submodule update --init --recursive
 
-# build the library using the NDK
+# build the native libraries using the NDK
 (
 cd ics-openvpn/main || exit
 ./misc/build-native.sh
@@ -52,6 +53,6 @@ cd ics-openvpn/main || exit
 ./gradlew clean assembleRelease
 #./gradlew clean assembleDebug
 
+# generate a keystore
+#keytool -genkey -v -keystore ~/my-release-key.jks
 #apksigner sign --ks ~/my-release-key.jks app/build/outputs/apk/app-debug.apk
-
-#sudo cp app/build/outputs/apk/app-debug.apk /var/www/html/
