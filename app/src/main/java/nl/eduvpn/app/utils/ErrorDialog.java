16984751cc263add0f4ffb3005cc1f2300c116a8
Dániel Zolnai <zoiddani@gmail.com>
2018-05-02 11:49:41 +0200

    [#141] Handle revoked client from profile

--- app/src/main/java/nl/eduvpn/app/utils/ErrorDialog.java
+++ app/src/main/java/nl/eduvpn/app/utils/ErrorDialog.java
@@ -19,6 +19,7 @@ package nl.eduvpn.app.utils;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.support.annotation.CheckResult;
 import android.support.annotation.StringRes;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -54,8 +55,9 @@ public class ErrorDialog {
      * @param title   The title of the dialog (string resource ID).
      * @param message The message to show as a string.
      */
-    public static void show(Context context, @StringRes int title, String message) {
-        show(context, context.getString(title), message, null);
+    @CheckResult
+    public static Dialog show(Context context, @StringRes int title, String message) {
+        return show(context, context.getString(title), message, null);
     }
 
     /**
@@ -65,8 +67,9 @@ public class ErrorDialog {
      * @param title   The title of the dialog (string resource ID).
      * @param message The message to show (string resource ID).
      */
-    public static void show(Context context, @StringRes int title, @StringRes int message) {
-        show(context, context.getString(title), context.getString(message), null);
+    @CheckResult
+    public static Dialog show(Context context, @StringRes int title, @StringRes int message) {
+        return show(context, context.getString(title), context.getString(message), null);
     }
 
     /**
@@ -76,7 +79,8 @@ public class ErrorDialog {
      * @param title   The title of the dialog.
      * @param message The message to show.
      */
-    public static void show(Context context, String title, String message, final InstanceWarningHandler handler) {
+    @CheckResult
+    public static Dialog show(Context context, String title, String message, final InstanceWarningHandler handler) {
         final Dialog dialog = new Dialog(context, R.style.ErrorDialog);
         dialog.setCanceledOnTouchOutside(true);
         dialog.setContentView(R.layout.dialog_error);
@@ -117,12 +121,8 @@ public class ErrorDialog {
         Button confirmButton = view.findViewById(R.id.confirmButton);
         titleView.setText(title);
         errorTextView.setText(message);
-        confirmButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dialog.dismiss();
-            }
-        });
+        confirmButton.setOnClickListener(v -> dialog.dismiss());
         dialog.show();
+        return dialog;
     }
 }
