d780742b7517e0876ac6aff6ed5ea1e7418cb643
François Kooman <fkooman@tuxed.net>
2018-06-28 12:37:24 +0200

    fix LC build

--- build_app.sh
+++ build_app.sh
@@ -20,7 +20,7 @@ SIGNED_APK=${PROJECT_DIR}/eduVPN-${GIT_TAG}.apk
 
 # Let's Connect!
 #GRADLE_TASK=app:assembleHomeRelease
-#UNSIGNED_APK=${APP_DIR}/app/build/outputs/apk/basic/release/app-home-release-unsigned.apk 
+#UNSIGNED_APK=${APP_DIR}/app/build/outputs/apk/home/release/app-home-release-unsigned.apk 
 #SIGNED_APK=${PROJECT_DIR}/LetsConnect-${GIT_TAG}.apk
 
 ###############################################################################
