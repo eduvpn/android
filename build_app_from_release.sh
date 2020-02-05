95dc63052d6591cca54a2b51d70fd64943671f99
François Kooman <fkooman@tuxed.net>
2020-02-05 18:27:47 +0100

    add release build script

---
+++ build_app_from_release.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+###############################################################################
+# CONFIGURATION
+###############################################################################
+
+SDK_DIR=${HOME}/android-sdk
+KEY_STORE=${HOME}/android.jks
+
+V=1.3.0
+DOWNLOAD_URL=https://github.com/eduvpn/android/releases/download/${V}/eduvpn-android-${V}.tar.xz
+
+PROJECT_DIR=${HOME}/Projects
+APP_DIR=${PROJECT_DIR}/eduvpn-android-${V}
+
+# eduVPN
+GRADLE_TASK=app:assembleBasicRelease
+UNSIGNED_APK=${APP_DIR}/app/build/outputs/apk/basic/release/app-basic-release-unsigned.apk 
+SIGNED_APK=${PROJECT_DIR}/eduVPN-${V}.apk
+
+# Let's Connect!
+#GRADLE_TASK=app:assembleHomeRelease
+#UNSIGNED_APK=${APP_DIR}/app/build/outputs/apk/home/release/app-home-release-unsigned.apk 
+#SIGNED_APK=${PROJECT_DIR}/LetsConnect-${V}.apk
+
+###############################################################################
+# CLONE
+###############################################################################
+
+(
+    mkdir -p "${PROJECT_DIR}"
+    cd "${PROJECT_DIR}" || exit
+
+    curl -L -o eduvpn-android-${V}.tar.xz "${DOWNLOAD_URL}"
+    tar -xJf eduvpn-android-${V}.tar.xz
+)
+
+###############################################################################
+# BUILD
+###############################################################################
+
+(
+    export ANDROID_HOME=${SDK_DIR}
+    cd "${APP_DIR}" || exit
+    ./gradlew ${GRADLE_TASK} --warning-mode all --stacktrace || exit
+)
+
+###############################################################################
+# SIGN
+###############################################################################
+
+(
+    # pick the newest build tools in case multiple versions are available
+    BUILD_TOOLS_VERSION=$(ls ${SDK_DIR}/build-tools/ | sort -r | head -1)
+    ${SDK_DIR}/build-tools/${BUILD_TOOLS_VERSION}/apksigner sign --ks "${KEY_STORE}" "${UNSIGNED_APK}" || exit
+    cp "${UNSIGNED_APK}" "${SIGNED_APK}" || exit
+)
