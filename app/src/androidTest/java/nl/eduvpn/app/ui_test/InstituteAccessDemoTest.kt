8241dd812f82f37ba46df25e97ff3199d3839229
Dániel Zolnai <zoiddani@gmail.com>
2020-09-07 14:13:47 +0200

    Add more tests

--- app/src/androidTest/java/nl/eduvpn/app/ui_test/InstituteAccessDemoTest.kt
+++ app/src/androidTest/java/nl/eduvpn/app/ui_test/InstituteAccessDemoTest.kt
@@ -18,52 +18,53 @@
 
 package nl.eduvpn.app.ui_test
 
-import androidx.test.espresso.Espresso.closeSoftKeyboard
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.isFocusable
-import androidx.test.espresso.matcher.ViewMatchers.withClassName
 import androidx.test.espresso.matcher.ViewMatchers.withHint
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
-import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
-import androidx.test.uiautomator.By
+import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.uiautomator.UiDevice
 import androidx.test.uiautomator.UiObjectNotFoundException
 import androidx.test.uiautomator.UiScrollable
 import androidx.test.uiautomator.UiSelector
-import androidx.test.uiautomator.Until
 import nl.eduvpn.app.BaseRobot
 import nl.eduvpn.app.BuildConfig
 import nl.eduvpn.app.MainActivity
+import nl.eduvpn.app.R
 import nl.eduvpn.app.utils.Log
 import nl.eduvpn.app.waitUntilGone
-import org.hamcrest.CoreMatchers.containsString
-import org.hamcrest.core.AllOf.allOf
+import org.hamcrest.CoreMatchers
+import org.hamcrest.core.AllOf
+import org.junit.Assert.fail
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 /**
- * Tests if the flow of connecting to a VPN provider fully works.
- * This test only works both on eduVPN and LetsConnect.
+ * Tests if searching for a provider based upon a secondary keyword is successful.
+ * This test only works on eduVPN.
  */
 @RunWith(AndroidJUnit4::class)
 @LargeTest
-class ConnectVpnTest {
+class InstituteAccessDemoTest {
 
     companion object {
-        private val TAG = ConnectVpnTest::class.java.name
+        private val TAG = InstituteAccessDemoTest::class.java.name
 
-        private const val TEST_SERVER_URL = "vpntest.spoor.nu"
+        private const val DEMO_ORGANIZATION_NAME = "Demo"
 
-        private const val TEST_SERVER_USERNAME = "google"
-        private const val TEST_SERVER_PASSWORD = "auto-test"
+        private const val DEMO_USER = "daniel+demo@egeniq.com"
+        private const val DEMO_PASSWORD = "AndroidTest123"
     }
 
     @get:Rule
@@ -72,45 +73,48 @@ class ConnectVpnTest {
     private val isLetsConnect = BuildConfig.FLAVOR == "home"
 
     @Test
-    fun testVpnConnectFlow() {
+    fun testAddDemoServer() {
         if (isLetsConnect) {
-            onView(withHint("Enter server address here")).perform(typeText(TEST_SERVER_URL))
-            closeSoftKeyboard()
-            onView(withText("ADD SERVER")).perform(click())
-        } else {
-            // Wait for list to load
-            try {
-                onView(withText("Fetching organizations...")).perform(waitUntilGone(2_000L))
-            } catch (ex: Exception) {
-                Log.i(TAG, "Couldn't find loading popup")
-            }
-            BaseRobot().waitForView(withText("Institute Access"), waitMillis = 2_000).check(matches(isDisplayed()))
-            onView(withHint("Search for your organization...")).perform(typeText(TEST_SERVER_URL))
-            closeSoftKeyboard()
-            BaseRobot().waitForView(
-                    allOf(withText(TEST_SERVER_URL), withClassName(containsString("TextView")))
-            ).perform(click())
+            fail("This test only works on the EduVPN app!")
+            return
         }
+        // Wait for list to load
+        try {
+            onView(withText("Fetching organizations...")).perform(waitUntilGone(2_000L))
+        } catch (ex: Exception) {
+            Log.i(TAG, "Couldn't find loading popup")
+        }
+        BaseRobot().waitForView(withText("Institute Access"), waitMillis = 2_000).check(matches(isDisplayed()))
+        onView(withHint("Search for your organization...")).perform(typeText(DEMO_ORGANIZATION_NAME), closeSoftKeyboard())
+        BaseRobot().waitForView(
+                AllOf.allOf(withText(DEMO_ORGANIZATION_NAME), ViewMatchers.withClassName(CoreMatchers.containsString("TextView")))
+        ).perform(click())
         // Switch over to UI Automator now, to control the browser
-        val device = UiDevice.getInstance(getInstrumentation())
+        val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
         val selector = UiSelector()
         // Wait for the browser to open and load
         Thread.sleep(2_000L)
         try {
+            // Select eduID from the list
+            val eduIDButton = device.findObject(selector.text("eduID"))
+            eduIDButton.click()
             // We can't find objects based on hints here, so we do it on layout order instead.
-            Log.v(TAG, "Entering username.")
+            Log.v(TAG, "Entering email address.")
             val userName = device.findObject(selector.className("android.widget.EditText").instance(0))
             userName.click()
-            userName.text = TEST_SERVER_USERNAME
+            userName.text = DEMO_USER
+            device.pressBack()
+            Log.v(TAG, "Clicking 'type password anyway' link")
+            val typePasswordLink = device.findObject(selector.text("Type a password anyway"))
+            typePasswordLink.click()
             Log.v(TAG, "Entering password.")
             val password = device.findObject(selector.className("android.widget.EditText").instance(1))
             password.click()
-            password.text = TEST_SERVER_PASSWORD
-            Log.v(TAG, "Hiding keyboard.")
-            device.pressBack() // Closes the keyboard
-            Log.v(TAG, "Signing in.")
-            val signInButton = device.findObject(selector.className("android.widget.Button").text("Sign In"))
-            signInButton.click()
+            password.text = DEMO_PASSWORD
+            device.pressBack()
+            Log.v(TAG, "Logging in...")
+            val loginButton = device.findObject(selector.text("Login"))
+            loginButton.click()
         } catch (ex: UiObjectNotFoundException) {
             // Perhaps we are still logged in. In this case, we get to the approve page, so continue
             Log.w(TAG, "Object not found, user is perhaps logged in already.", ex)
@@ -130,18 +134,12 @@ class ConnectVpnTest {
         Log.v(TAG, "Approving VPN app.")
         val approveButton = device.findObject(selector.text("Approve"))
         approveButton.click()
-        // Wait for the app to display the server in its list
-        BaseRobot().waitForView(withText("vpntest.spoor.nu")).perform(click())
         try {
-            // We need the UI Automator again, because VPN accept permission is a system dialog
-            val isDialogShown = Until.findObject(By.text("Connection request"))
-            device.wait(isDialogShown, 3_000L)
-            val okButton = device.findObject(selector.className("android.widget.Button").text("OK"))
-            okButton.click()
+            approveButton.click() // Sometimes it doesn't work :)
         } catch (ex: Exception) {
-            Log.w(TAG, "No connection request shown, probably accepted already once.", ex)
+            // Unhandled
         }
-        BaseRobot().waitForView(withText("Connected")).check(matches(isDisplayed()))
+        BaseRobot().waitForView(withText("Demo")).check(matches(isDisplayed()))
     }
 
 }
