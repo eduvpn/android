83004931c1f2256b863fa609e029004469065c0f
Jasper Korten <jja2000@gmail.com>
2021-04-19 21:45:58 +0200

    builder_setup.sh: Replace Google SDK download with Android-Rebuilds SDK The script will now download rebuilt sdk tools needed to build the app instead of using sdk-manager. This means that there will not be any licenses to accept and there's no need to update links in the script when Google changes those.

--- builder_setup.sh
+++ builder_setup.sh
@@ -6,18 +6,20 @@
 
 SDK_DIR=${HOME}/android-sdk
 
-# current as of 20191028
-# see https://developer.android.com/studio/#downloads "Command line tools only"
-SDK_VERSION=6858069
+# Android-Rebuilds F-Droid Mirror, will have to be changed to something hosted by SURF or the original AR repo considering the NDK is already unavailable
+SDK_URL=https://mirror.f-droid.org/android-free/repository/
 
-# Always use latest from https://developer.android.com/studio/releases/build-tools
-BUILD_TOOLS_VERSION=29.0.3
+# Android-Rebuilds NDK mirror, temporary host because F-Droid does not host NDK version R21
+NDK_URL=https://aaio.eu/ndk/
+NDK_FILE=android-ndk-0-linux-x86_64.tar.bz2
 
-# see app/build.gradle for "targetSdkVersion"
-PLATFORM_VERSION=29
-
-# should not require modification...
-SDK_URL=https://dl.google.com/android/repository/commandlinetools-linux-${SDK_VERSION}_latest.zip
+declare -a arr=(
+    "sdk-repo-linux-tools-26.1.1.zip" # Includes sdkmanager
+    "sdk-repo-linux-platforms-eng.11.0.0_r27.zip" # To compile ics-openvpn
+    "sdk-repo-linux-platforms-eng.10.0.0_r36.zip" # To compile eduvpn
+    "sdk-repo-linux-platform-tools-eng.10.0.0_r14.zip" # Contains tools like adb and fastboot
+    "sdk-repo-linux-build-tools-eng.10.0.0_r14.zip" # Contains tools like apksigner
+)
 
 ###############################################################################
 # SETUP
@@ -25,24 +27,44 @@ SDK_URL=https://dl.google.com/android/repository/commandlinetools-linux-${SDK_VE
 
 # create and populate SDK directory
 (
-    mkdir -p "${SDK_DIR}"
+    mkdir -p "${SDK_DIR}" "${SDK_DIR}"/platforms "${SDK_DIR}"/build-tools "${SDK_DIR}"/ndk "${SDK_DIR}"/ndk/21.4.0
     cd "${SDK_DIR}" || exit
-    curl -L -O ${SDK_URL}
-    unzip -q commandlinetools-linux-${SDK_VERSION}_latest.zip
-    rm commandlinetools-linux-${SDK_VERSION}_latest.zip
-)
+    for i in "${arr[@]}"
+    do
+        echo "Downloading $i"
+        curl -L -O ${SDK_URL}/$i
 
-# accept licenses
-(
-    cd "${SDK_DIR}" || exit
-    yes | cmdline-tools/bin/sdkmanager --sdk_root=${SDK_DIR} --licenses
-)
+        echo "Unzipping $i"
+        unzip -q $i -d ${SDK_DIR}
+        rm $i
 
-# install required SDK components
-(
-    cd "${SDK_DIR}" || exit
-    cmdline-tools/bin/sdkmanager --sdk_root=${SDK_DIR} --update
-    cmdline-tools/bin/sdkmanager --sdk_root=${HOME}/android-sdk "ndk;21.0.6113669"
-    cmdline-tools/bin/sdkmanager --sdk_root=${HOME}/android-sdk "build-tools;${BUILD_TOOLS_VERSION}"
-    cmdline-tools/bin/sdkmanager --sdk_root=${HOME}/android-sdk "platforms;android-${PLATFORM_VERSION}"
+        # Some of these zips need to be either placed in subfolders or have to be
+        # renamed due to gradle warnings
+        if [ $i = sdk-repo-linux-tools-26.1.1.zip ]
+        then
+            mv tools/ cmdline-tools/
+        elif [ $i = sdk-repo-linux-build-tools-eng.10.0.0_r14.zip ]
+        then
+            mv android-10/ build-tools/29.0.2
+        elif [ $i = sdk-repo-linux-platforms-eng.10.0.0_r36.zip ]
+        then
+            mv android-10/ platforms/android-29
+        elif [ $i = sdk-repo-linux-platforms-eng.11.0.0_r27.zip ]
+        then
+            mv android-11/ platforms/android-30
+        fi
+
+        echo "Content of $i in place"
+        echo ""
+    done
+
+    echo "Downloading ${NDK_FILE}. This might take a while depending on your connection"
+    curl -L -O ${NDK_URL}/${NDK_FILE}
+    echo "Extracting ${NDK_FILE}. This might take a while depending on your system."
+    tar xjf ${NDK_FILE}
+    rm ${NDK_FILE}
+    mv android-ndk-r21e/* "${SDK_DIR}"/ndk/21.4.0/
+    rmdir android-ndk-r21e/
+    echo "Content of ${NDK_FILE} in place"
+    echo ""
 )
