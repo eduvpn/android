eduvpn/android

commit 942876402d3f1d312ef6304cd12c4cd366a38956

Use Handler instead of RxJava for updating cert expiry

app/src/main/java/nl/eduvpn/app/viewmodel/ConnectionStatusViewModel.kt
@@ -19,25 +19,16 @@
 package nl.eduvpn.app.viewmodel
 
 import android.content.Context
+import android.os.Handler
 import android.text.Spanned
 import androidx.core.text.HtmlCompat
 import androidx.lifecycle.MutableLiveData
 import de.blinkt.openvpn.VpnProfile
-import io.reactivex.Observable
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposable
 import nl.eduvpn.app.R
 import nl.eduvpn.app.entity.Profile
-import nl.eduvpn.app.service.APIService
-import nl.eduvpn.app.service.ConnectionService
-import nl.eduvpn.app.service.HistoryService
-import nl.eduvpn.app.service.PreferencesService
-import nl.eduvpn.app.service.SerializerService
-import nl.eduvpn.app.service.VPNService
-import nl.eduvpn.app.utils.Log
+import nl.eduvpn.app.service.*
 import nl.eduvpn.app.utils.getCountryText
 import nl.eduvpn.app.utils.toSingleEvent
-import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class ConnectionStatusViewModel @Inject constructor(
@@ -67,7 +58,11 @@ class ConnectionStatusViewModel @Inject constructor(
     private val _connectionParentAction = MutableLiveData<ParentAction>()
     val connectionParentAction = _connectionParentAction.toSingleEvent()
 
-    private var updateCertDisposable: Disposable? = null
+    private var updateCertHandler: Handler = Handler()
+    private var updateCertCallback: Runnable = Runnable {
+        updateCertExpiry()
+        runUpdateCertExpiryEverySecond()
+    }
 
 
     init {
@@ -97,33 +92,33 @@ class ConnectionStatusViewModel @Inject constructor(
 
     override fun onResume() {
         super.onResume()
-        updateCertDisposable = Observable.interval(1, TimeUnit.SECONDS)
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({
-                    updateCertExpiry()
-                }, { ex ->
-                    Log.e(TAG, "Unable to update cert expiry", ex)
-                })
+        runUpdateCertExpiryEverySecond()
     }
 
     fun onPause() {
-        updateCertDisposable?.dispose()
+        stopUpdateCertExpiry()
+    }
+
+    private fun runUpdateCertExpiryEverySecond() {
+        updateCertHandler.postDelayed(updateCertCallback, 1 * 1000)
+    }
+
+    private fun stopUpdateCertExpiry() {
+        updateCertHandler.removeCallbacks(updateCertCallback)
     }
 
     private fun updateCertExpiry() {
         val currentTime = System.currentTimeMillis()
         if (certExpiryTime == null) {
             // No cert or time, nothing to display
-            updateCertDisposable?.dispose()
-            updateCertDisposable = null
+            stopUpdateCertExpiry()
             certValidity.value = null
             return
         }
         val timeDifferenceInSeconds = (certExpiryTime - currentTime) / 1000
         if (timeDifferenceInSeconds < 0) {
             // Cert expired
-            updateCertDisposable?.dispose()
-            updateCertDisposable = null
+            stopUpdateCertExpiry()
             certValidity.value = HtmlCompat.fromHtml(context.getString(R.string.connection_certificate_status_expired), HtmlCompat.FROM_HTML_MODE_COMPACT)
             _connectionParentAction.value = ParentAction.SessionExpired
         } else if (timeDifferenceInSeconds < 60) {
@@ -159,7 +154,7 @@ class ConnectionStatusViewModel @Inject constructor(
         return vpnService.findMatchingVpnProfile(matchingSavedProfile)
     }
 
-    fun findCurrentProfile() : Profile? {
+    fun findCurrentProfile(): Profile? {
         return preferencesService.currentProfile
     }
 

app/build.gradle
@@ -173,9 +173,6 @@ dependencies {
     implementation 'com.squareup.picasso:picasso:2.71828'
     // Public key verification
     implementation "com.github.joshjdevl.libsodiumjni:libsodium-jni-aar:2.0.2@aar"
-    // Reactive extensions for asynchronous events
-    implementation 'io.reactivex.rxjava2:rxandroid:2.1.1'
-    implementation 'io.reactivex.rxjava2:rxjava:2.2.19'
 
     // Kotlin coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.1")

app/src/main/assets/licenses.html
@@ -1096,12 +1096,6 @@ Public License instead of this License.
         <a href="https://github.com/square/picasso">Picasso by Square</a>
     </li>
     <li>
-        <a href="https://github.com/ReactiveX/RxAndroid">RxAndroid by ReactiveX</a>
-    </li>
-    <li>
-        <a href="https://github.com/ReactiveX/RxJava">RxJava by ReactiveX</a>
-    </li>
-    <li>
         <a href="https://developer.android.com/reference/androidx/test/runner/package-summary">AndroidX
             Support Test Runner Library</a>
     </li>

commit f27b25c6cf416a3fbd8febf1738fab94fd1442a8

Fix certificate expired notification every second
Fixes https://github.com/eduvpn/android/issues/329 caused by
942876402d3f1d312ef6304cd12c4cd366a38956.

app/src/main/java/nl/eduvpn/app/viewmodel/ConnectionStatusViewModel.kt
@@ -60,10 +60,10 @@ class ConnectionStatusViewModel @Inject constructor(
 
     private var updateCertHandler: Handler = Handler()
     private var updateCertCallback: Runnable = Runnable {
-        updateCertExpiry()
-        runUpdateCertExpiryEverySecond()
+        if(updateCertExpiry()) {
+            runUpdateCertExpiryEverySecond()
+        }
     }
-
 
     init {
         refreshProfile()
@@ -107,20 +107,22 @@ class ConnectionStatusViewModel @Inject constructor(
         updateCertHandler.removeCallbacks(updateCertCallback)
     }
 
-    private fun updateCertExpiry() {
+    /**
+     * @return If the cert expiry time should continue to be updated.
+     */
+    private fun updateCertExpiry(): Boolean {
         val currentTime = System.currentTimeMillis()
         if (certExpiryTime == null) {
             // No cert or time, nothing to display
-            stopUpdateCertExpiry()
             certValidity.value = null
-            return
+            return false
         }
         val timeDifferenceInSeconds = (certExpiryTime - currentTime) / 1000
         if (timeDifferenceInSeconds < 0) {
             // Cert expired
-            stopUpdateCertExpiry()
             certValidity.value = HtmlCompat.fromHtml(context.getString(R.string.connection_certificate_status_expired), HtmlCompat.FROM_HTML_MODE_COMPACT)
             _connectionParentAction.value = ParentAction.SessionExpired
+            return false
         } else if (timeDifferenceInSeconds < 60) {
             // Expires within a minute
             val seconds = context.resources.getQuantityString(R.plurals.certificate_status_seconds, timeDifferenceInSeconds.toInt(), timeDifferenceInSeconds)
@@ -145,6 +147,7 @@ class ConnectionStatusViewModel @Inject constructor(
             val days = context.resources.getQuantityString(R.plurals.certificate_status_days, timeDifferenceInSeconds.div(3600 * 24).toInt(), timeDifferenceInSeconds.div(3600 * 24).toInt())
             certValidity.value = HtmlCompat.fromHtml(context.getString(R.string.connection_certificate_status_valid_for_one_part, days), HtmlCompat.FROM_HTML_MODE_COMPACT)
         }
+        return true
     }
 
     fun findCurrentConfig(): VpnProfile? {