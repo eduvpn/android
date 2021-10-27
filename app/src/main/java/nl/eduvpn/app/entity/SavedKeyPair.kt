878c240ef7060ddeddde6782ca0182852eadf167
Nick Aquina <nickaquina@gmail.com>
2021-10-27 16:29:25 +0200

    Convert SavedKeyPair to Kotlin

--- app/src/main/java/nl/eduvpn/app/entity/SavedKeyPair.kt
+++ app/src/main/java/nl/eduvpn/app/entity/SavedKeyPair.kt
@@ -14,27 +14,10 @@
  *     You should have received a copy of the GNU General Public License
  *     along with eduVPN.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-package nl.eduvpn.app.entity;
+package nl.eduvpn.app.entity
 
 /**
  * Extends the keypair with a base URI which we add.
  * Created by Daniel Zolnai on 2017-08-01.
  */
-public class SavedKeyPair {
-    private Instance _instance;
-    private KeyPair _keyPair;
-
-    public SavedKeyPair(Instance instance, KeyPair keyPair) {
-        _instance = instance;
-        _keyPair = keyPair;
-    }
-
-    public Instance getInstance() {
-        return _instance;
-    }
-
-    public KeyPair getKeyPair() {
-        return _keyPair;
-    }
-}
+data class SavedKeyPair(val instance: Instance, val keyPair: KeyPair)
