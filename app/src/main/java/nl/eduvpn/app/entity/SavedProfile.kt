9b7ace77f499d7fec58c9becfb7242dd25014740
Nick Aquina <nickaquina@gmail.com>
2021-10-27 11:39:23 +0200

    Convert SavedProfile to Kotlin

--- app/src/main/java/nl/eduvpn/app/entity/SavedProfile.kt
+++ app/src/main/java/nl/eduvpn/app/entity/SavedProfile.kt
@@ -14,34 +14,10 @@
  *     You should have received a copy of the GNU General Public License
  *     along with eduVPN.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-package nl.eduvpn.app.entity;
+package nl.eduvpn.app.entity
 
 /**
  * Stores the data of a saved profile.
  * Created by Daniel Zolnai on 2016-10-20.
  */
-public class SavedProfile {
-
-    private Instance _instance;
-    private Profile _profile;
-    private String _profileUUID;
-
-    public SavedProfile(Instance instance, Profile profile, String profileUUID) {
-        _instance = instance;
-        _profile = profile;
-        _profileUUID = profileUUID;
-    }
-
-    public Instance getInstance() {
-        return _instance;
-    }
-
-    public Profile getProfile() {
-        return _profile;
-    }
-
-    public String getProfileUUID() {
-        return _profileUUID;
-    }
-}
+data class SavedProfile(val instance: Instance, val profile: Profile, val profileUUID: String)
