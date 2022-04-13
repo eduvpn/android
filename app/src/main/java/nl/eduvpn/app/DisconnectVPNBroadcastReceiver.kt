fd5171d97fd6e79ceb8d99e7ded69f1b1c01c718
Nick Aquina <nickaquina@gmail.com>
2022-04-13 21:28:20 +0200

    Make injected services protected in DisconnectVPNBroadcastReceiver

--- app/src/main/java/nl/eduvpn/app/DisconnectVPNBroadcastReceiver.kt
+++ app/src/main/java/nl/eduvpn/app/DisconnectVPNBroadcastReceiver.kt
@@ -10,10 +10,10 @@ import javax.inject.Inject
 class DisconnectVPNBroadcastReceiver : BroadcastReceiver() {
 
     @Inject
-    lateinit var vpnConnectionService: VPNConnectionService
+    protected lateinit var vpnConnectionService: VPNConnectionService
 
     @Inject
-    lateinit var vpnService: VPNService
+    protected lateinit var vpnService: VPNService
 
     companion object {
         val ACTION = this::class.qualifiedName!!.plus(".disconnect_vpn")
