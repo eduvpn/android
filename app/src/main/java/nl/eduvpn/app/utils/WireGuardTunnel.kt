f107e1f7c29af6773bf4ccbe11fd9582d060dd34
Nick Aquina <nickaquina@gmail.com>
2022-04-13 23:14:09 +0200

    Make onStateChangeFunction private in WireGuardTunnel

--- app/src/main/java/nl/eduvpn/app/utils/WireGuardTunnel.kt
+++ app/src/main/java/nl/eduvpn/app/utils/WireGuardTunnel.kt
@@ -2,7 +2,10 @@ package nl.eduvpn.app.utils
 
 import com.wireguard.android.backend.Tunnel
 
-class WireGuardTunnel(private val name: String, val onStateChangeFunction: (newState: Tunnel.State) -> Unit) : Tunnel {
+class WireGuardTunnel(
+    private val name: String,
+    private val onStateChangeFunction: (newState: Tunnel.State) -> Unit
+) : Tunnel {
 
     override fun getName() = name
 
